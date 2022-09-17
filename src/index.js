c2b35cc47c9641ba08095abeeb885aa6763166ff
martinzeeden <martin.zeeden@outlook.de>
2022-09-17 11:43:54 +0200

    add firebase

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import FirebaseContext from './context/firebase'
+import { firebase, FieldValue } from './lib/firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </React.StrictMode>
+  </FirebaseContext.Provider>
 );
