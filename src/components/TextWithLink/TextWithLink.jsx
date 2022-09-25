6a0b32253bb09e5acda96cbd6049ffcb56d10f5a
martinzeeden <martin.zeeden@outlook.de>
2022-09-25 17:09:57 +0200

    update link

--- src/components/TextWithLink/TextWithLink.jsx
+++ src/components/TextWithLink/TextWithLink.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './TextWithLink.module.css';
 
 const TextWithLink = ({ text, link, linkText }) => {
   return (
     <p>
-      {text} <Link to={link}>{linkText}</Link>
+      {text}{' '}
+      <Link className={styles.link} to={link}>
+        {linkText}
+      </Link>
     </p>
   );
 };
