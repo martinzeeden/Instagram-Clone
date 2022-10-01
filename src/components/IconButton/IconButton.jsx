3c77ab69ec176d90e816dcae93a928990dd51f66
martinzeeden <martin.zeeden@outlook.de>
2022-10-01 14:07:09 +0200

    update IconButton

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './IconButton.module.css';
 
-const IconButton = ({ icon, onClick, disabled }) => (
-  <button className={styles.iconButton}>
+const IconButton = ({ icon, onClick, disabled = false }) => (
+  <button className={styles.iconButton} onClick={() => onClick?.()} disabled={disabled}>
     <img className={styles.icon} src={icon} alt="Icon"/>
   </button>
 );
