f8178852814c537ea8cfedb72df1008d752f284b
martinzeeden <martin.zeeden@outlook.de>
2022-10-01 14:07:38 +0200

    add userContext and userAuthListener

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import './variables.css';
 import * as ROUTES from './constants/Routes';
+import useAuthListener from './hooks/useAuthListener';
+import UserContext from './context/user';
 
 const Login = lazy(() => import('./pages/Login/Login'));
 const Register = lazy(() => import('./pages/Register/Register'));
@@ -11,18 +13,22 @@ const Profile = lazy(() => import('./pages/Profile/Profile'));
 const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
 
 const App = () => {
+  const { user } = useAuthListener();
+
   return (
-    <BrowserRouter>
-      <Suspense fallback={<p>Loading ...</p>}>
-        <Routes>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.REGISTER} element={<Register />} />
-          <Route path={ROUTES.DASHBOARD} element={<Dashboard/>}/>
-          <Route path={ROUTES.PROFILE} element={<Profile/>}/>
-          <Route path="*" element={<NotFound/>}/>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <UserContext.Provider value={{ user}}>
+      <BrowserRouter>
+        <Suspense fallback={<p>Loading ...</p>}>
+          <Routes>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.REGISTER} element={<Register />} />
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard/>}/>
+            <Route path={ROUTES.PROFILE} element={<Profile/>}/>
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 };
 
