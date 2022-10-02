8ccb82f29fa5016f1c8cf95d7a3c9340ebd96197
martinzeeden <martin.zeeden@outlook.de>
2022-10-02 18:50:58 +0200

    add avatar to header

--- src/components/DashboardHeader/DashboardHeader.jsx
+++ src/components/DashboardHeader/DashboardHeader.jsx
@@ -5,19 +5,33 @@ import IconHome from '../../assets/icons/home.svg';
 import IconLogout from '../../assets/icons/logout.svg';
 import IconSettings from '../../assets/icons/settings.svg';
 import IconButton from '../IconButton/IconButton';
+import { useContext } from 'react';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
+import { useNavigate } from 'react-router-dom';
+import { DASHBOARD } from '../../constants/Routes';
 
-const DashboardHeader = () => (
-  <header className={styles.headerContainer}>
-    <div className={styles.header}>
-      <h1>Instagram</h1>
-      <div className={styles.buttonBox}>
-        <IconButton icon={IconAdd}/>
-        <IconButton icon={IconHome}/>
-        <IconButton icon={IconSettings}/>
-        <IconButton icon={IconLogout} />
+const DashboardHeader = () => {
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.header}>
+        <h1>Instagram</h1>
+        <div className={styles.buttonBox}>
+          {/* <IconButton icon={IconAdd}/> */}
+          <IconButton icon={IconHome} onClick={() => navigate(DASHBOARD)}/>
+          <IconButton icon={IconSettings}/>
+          <IconButton icon={IconLogout} onClick={() => firebase.auth().signOut()}/>
+          <button className={styles.avatar} onClick={() => navigate(`profile/${user.displayName}`)}>
+            <img src={`images/avatars/${"martin"}.jpg`}alt=""/>
+          </button>
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default DashboardHeader;
