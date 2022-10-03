558801d64be7f939f6348b97805b61c1de37e685
martinzeeden <martin.zeeden@outlook.de>
2022-10-03 11:53:07 +0200

    add profileHeader component

---
+++ src/pages/Profile/ProfileHeader/ProfileHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import FollowButton from '../../../components/FollowButton/FollowButton';
+import styles from './ProfileHeader.module.css';
+
+const ProfileHeader = ({
+  profileImage, username, disableFollowing, canUnfollow, picturesNumber, followersNumber, followingNumber, description
+}) => {
+  return (
+    <div className={styles.topContainer}>
+      <img src={profileImage} alt="Profilepicture" className={styles.profileImage}/>
+      <div className={styles.nameContainer}>
+        <div className={styles.usernameContainer}>
+          <h3 className={styles.username}>{username}</h3>
+          {!disableFollowing && (<FollowButton canUnfollow={canUnfollow}/>)}
+        </div>
+        <div className={styles.profileStatsContainer}>
+          <p><b>{picturesNumber}</b> Pictures</p>
+          <p><b>{followersNumber}</b> Follower</p>
+          <p><b>{followingNumber}</b> Following</p>
+        </div>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileHeader;
