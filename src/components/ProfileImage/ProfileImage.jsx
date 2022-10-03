2081fcdac6d8e187517b51f5677f84c15593b762
martinzeeden <martin.zeeden@outlook.de>
2022-10-03 11:52:29 +0200

    add ProfileImage component

---
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './ProfileImage.module.css';
+import HeartIcon from '../../assets/icons/heart.svg';
+import ChatIcon from '../../assets/icons/chat.svg';
+
+const ProfileImage = ({ image, likes, comments }) => (
+  <div className={styles.imageOverlayContainer}>
+    <div className={styles.imageOverlay}>
+      <div className={styles.imageOverlayItem}>
+        <img src={HeartIcon} alt="Like Icon"/>
+        <p>{likes}</p>
+      </div>
+      <div className={styles.imageOverlayItem}>
+        <img src={ChatIcon} alt="Comment Icon"/>
+        <p>{comments}</p>
+      </div>
+    </div>
+    <img src={image} className={styles.image} alt="Upload"/>
+  </div>
+);
+
+export default ProfileImage;
