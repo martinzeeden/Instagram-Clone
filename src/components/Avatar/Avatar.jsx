c1a082472cd7a5e7684dd3418d24b39e27237a10
martinzeeden <martin.zeeden@outlook.de>
2022-11-02 19:46:34 +0100

    update avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Avatar.module.css';
+import accountImage from '../../assets/icons/account.svg'
 
 const Avatar = ({ user }) => {
   const navigate = useNavigate();
 
   return (
     <button className={styles.avatar} onClick={() => navigate(`profile/${user.displayName}`)}>
-      <img src={`/images/avatars/${"martin"}.jpg`} alt=""/>
+      <img src={accountImage} alt=""/>
     </button>
   );
 }
